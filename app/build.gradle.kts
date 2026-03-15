8737c33001ab5006dd44be0a54408547b2e31578
JDSA Ling <CutelinXXX@126.com>
2026-03-15 22:47:52 +0800

    0.1.3更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.example.pdtranslator"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 5
+        versionName = "0.1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
